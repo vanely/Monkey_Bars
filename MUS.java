2d864e641ef25d4ce46b2c5cdfb9baec22672241
vanely <vanelys94@gmail.com>
2017-12-20 15:11:39 -0500

    word change

--- MUS.java
+++ MUS.java
@@ -2,6 +2,6 @@ public class Mus
 {
 	public static void main(String[] args)
 	{
-	    System.out.println("Let's swing on them curley braces!")
+	    System.out.println("Let's swing on some curley braces!")
 	}
 }
