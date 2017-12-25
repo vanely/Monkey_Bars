807a8f6fd489e73d4503cf6ada3266dd3430a417
Osa <o3taway@gmail.com>
2017-12-25 01:50:17 -0500

    Added functionality for other scales, and made a dictionary to hold the spacing of scales.

--- src/Theory.java
+++ src/Theory.java
@@ -1,11 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
 
 public class Theory {
-    public static List<Note> getMajorScale(int midiNoteNumber) {
+    private static final Map<String, int[]> scales = Map.ofEntries(
+            entry("Major",          new int[]{ 0, 2, 4, 5, 7, 9, 11, 12}),
+            entry("NaturalMinor",   new int[]{ 0, 2, 3, 5, 7, 8, 10, 12}),
+            entry("HarmonicMinor",  new int[]{ 0, 2, 3, 5, 7, 8, 11, 12}),
+            entry("MelodicMinor",  new int[]{ 0, 2, 3, 5, 7, 9, 11, 12})
+    );
+
+    private static final Map<String, String> scalesSpacing = Map.ofEntries(
+            entry("Major",          "RWWHWWWH"),
+            entry("NaturalMinor",   "RWHWWHWW"),
+            entry("HarmonicMinor",  "RWHWWH3H"),
+            entry("MelodicMinor",   "RWHWWWWH")
+
+    );
+
+    public static Scale getMajorScale(int midiNoteNumber) {
         Scale majorScale = new Scale();
 
-        int[] major = { 0, 2, 4, 5, 7, 9, 11, 12};
+        int[] major = scales.get("Major");
 
         for (int i = 0; i < major.length; i++) {
             majorScale.add(new Note(midiNoteNumber+major[i]));
@@ -14,6 +32,17 @@ public class Theory {
         return majorScale;
     }
 
+    public static Scale getNaturalMinorScale(int midiNoteNumber) {
+        Scale naturalMinorScale = new Scale();
+
+        int[] naturalMinor = scales.get("NaturalMinor");
+
+        for (int i = 0; i < naturalMinor.length; i++) {
+            naturalMinorScale.add(new Note(midiNoteNumber+naturalMinor[i]));
+        }
+
+        return naturalMinorScale;
+    }
     // TODO: create this method that will turn the scale generate into a scale with proper musical spelling.
     /*
     public static Scale normalizeScale(Scale original) {
