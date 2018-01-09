82d5104f0fcf02ac0f8d5a84765d3c7579df2f67
Osa <o3taway@gmail.com>
2018-01-08 23:16:32 -0500

    Created a test folder and the shell of test methods for use with testing using JUnit5.

---
+++ test/TheoryTest.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testCreateScalesSpacingList() {
+    }
+
+    @Test
+    void testCreateModesList() {
+    }
+
+    @Test
+    void testGetRomanNumeral() {
+    }
+
+    @Test
+    void testGetOctaveBaseList() {
+    }
+
+    @Test
+    void testGetScale() {
+    }
+
+    @Test
+    void testGetScale1() {
+    }
+
+    @Test
+    void testGetBaseEquivalent() {
+    }
+
+    @Test
+    void testGetSingleFlatEquivalent() {
+    }
+
+    @Test
+    void testGetFlatishEquivalent() {
+    }
+
+    @Test
+    void testGetSingleSharpEquivalent() {
+    }
+
+    @Test
+    void testGetSharpishEquivalent() {
+    }
+
+    @Test
+    void testGetCircleLabel() {
+    }
+
+    @Test
+    void testGetMajorScale() {
+    }
+
+    @Test
+    void testGetNaturalMinorScale() {
+    }
+
+    @Test
+    void testGetBase() {
+    }
+
+    @Test
+    void testNormalizeScale() {
+    }
+}
